3c8524c5d2a677f7f9e3a86d10bbdd72e005b6dd
yonghanyoon <yoonyh95@naver.com>
2025-04-04 15:04:57 +0900

    init

---
+++ src/main/java/org/musinsa/stylist/StylistApplication.java
@@ -0,0 +1,13 @@
+package org.musinsa.stylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StylistApplication.class, args);
+    }
+
+}
